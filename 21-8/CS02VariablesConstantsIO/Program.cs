baa22d37fa16a1f64a69c1352120bafa5f91c460
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-08-23 17:46:24 +0700

    docx.txt update

---
+++ 21-8/CS02VariablesConstantsIO/Program.cs
@@ -0,0 +1,16 @@
+﻿// khai bao
+string FullName = "nguyen minh phuc";
+// khai bao sau do gan gt
+int age;
+age = 18;
+// in ra 
+Console.WriteLine("name {0}, age {1}", FullName, age);
+// nhap gt
+Console.Write("Nhập tuoi : ");
+age = Convert.ToInt32(Console.ReadLine()); // ep kieu
+// var
+var address = "ha noi"; // string
+// hang
+const double PI = 3.14;
+
+
